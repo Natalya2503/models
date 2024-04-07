8c24667b6e608f15c1079d5c196e980d58d3f92f
Natalya2503 <147742964+Natalya2503@users.noreply.github.com>
2024-04-07 15:52:09 +0300

    Update views.py

--- views.py
+++ views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Information
 
-posts = ['Новые тренды в моде', 'Лучшие места для отпуска', 'Интересные факты о животных']
+# posts = ['Новые тренды в моде', 'Лучшие места для отпуска', 'Интересные факты о животных']
 
-def index(request):
-    data = {
-        'name': 'Александр',
-        'last_name': 'Белов',
-        'email': 'belov@jmail.com',
-        'birthday': '12.09.1994',
-        'posts': posts
-    }
-    return render (request, 'index.html', context=data)
+# def index(request):
+#     data = {
+#         'name': 'Александр',
+#         'last_name': 'Белов',
+#         'email': 'belov@jmail.com',
+#         'birthday': '12.09.1994',
+#         'posts': posts
+#     }
+#     return render (request, 'index.html', context=data)
 
-def contacts(request):
-    return render (request, 'contact.html' )
+# def contacts(request):
+#     return render (request, 'contact.html' )
 
 
 def users(request):
